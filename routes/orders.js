ffe05abac471897c95b19f27f3d1a0d4e86a3cbc
furqan.maudhudy <furqanmaudhudy@gmail.com>
2019-01-31 13:50:39 +0700

    create orders routes

---
+++ routes/orders.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const router = express.Router()
+
+const OrderController = require('../controllers/OrderController')
+const isAuthenticated = require('../middlewares').isAuthenticated
+
+router
+  .route('/:id')
+  .post(OrderController.CreateOrderById)
+
+module.exports = router
