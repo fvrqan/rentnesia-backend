74f439b25255404f4c61abbd72f9a4fe5f456956
furqan.maudhudy <furqanmaudhudy@gmail.com>
2019-01-31 13:50:15 +0700

    Create order by id

---
+++ controllers/OrderController.js
@@ -0,0 +1,19 @@
+const Order = require('../models').order
+
+// Add new order
+exports.CreateOrderById = async (req, res) => {
+  try {
+    await Order.create(
+      {
+        address: req.body.address,
+        pick_up: req.body.pick_up,
+        notes: req.body.notes
+      },
+      { where: { id: req.params.id } }
+    )
+    const order = await Order.findById(req.params.id)
+    res.json({ order })
+  } catch (error) {
+    res.json(error)
+  }
+}
